48e9905220a5c1e3187974c80c764448128653d5
Jakub Małyjasiak <malyjasiak@gmail.com>
2017-11-29 17:42:07 +0100

    Add basic exception handling.

---
+++ backend/src/main/java/com/theeye/api/v1/common/model/ErrorCodes.java
@@ -0,0 +1,15 @@
+package com.theeye.api.v1.common.model;
+
+public class ErrorCodes {
+
+     public static final String IMAGE_PREFIX = "1";
+     public static final String BOARD_PREFIX = "2";
+     public static final String ENGINE_PREFIX = "3";
+     public static final String FEN_PREFIX = "4";
+
+     public static final String PATTERN_NOT_FOUND = "01";
+     public static final String INCORRECT_BOUNDS = "02";
+     public static final String INVALID_FEN = "01";
+     public static final String MOVE_DETECTION = "01";
+
+}
