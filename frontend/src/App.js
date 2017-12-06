0c0f834c41b1110e8b9ad8b4c49be0469bb5543d
Jakub Małyjasiak <malyjasiak@gmail.com>
2017-12-06 23:30:10 +0100

    Add frontent view layer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,18 +21,77 @@ class App extends Component {
   };
 
   render() {
-    return (
-      <div className="App">
-          <header> Chessify </header>
-          <Chessboard position={this.currentPosition}/>
-          <button
+      return (
+          <div className="App">
+              <header> <h2>The Eye</h2> </header>
+              <div class="container">
+                  <div class="row top-margin">
+                      <div class="col-md-5 col-md-offset-2">
+                              <button class="btn btn-green" title="Take a snapshot of epmty chessboard to let us configure tiles coordinates">Configure coords</button>
+                              <button class="btn btn-grey btn-following" title="Take a snapshot of board with all pieces set up to let us get reference color samples">Configure colors</button>
+                              <button class="btn btn-grey btn-following" title="Take a snapshot every time you make a move">Play</button>
+                      </div>
+                      <div class="col-md-3">
+                          <button type="button" class="btn btn-green">Submit snapshot</button>
+                          <button type="button" class="btn btn-green btn-following">Finish</button>
+                      </div>
+                  </div>
+                  <div class="row top-margin">
+                      <div class="col-md-5 col-md-offset-2">
+                          <div class="advantage-gauge-holder">
+                              <div class="advantage-gauge"/>
+                              <div class="zero-gauge-marker"/>
+                          </div>
+                          <div class="chessdiagram-holder">
+                              <Chessboard/>
+                          </div>
+                      </div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <div class="webcam-holder">
+                                    <Webcam width={253} height={187} audio={false}/>
+                                </div>
+                            </div>
+                            <div class="moves-holder">
+                                <table class="table moves-table">
+                                    <thead class="moves-table-head">
+                                    <tr>
+                                        <th>Move</th>
+                                        <th class="middle-border">White</th>
+                                        <th>Black</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1.</td>
+                                        <td class="middle-border">e4</td>
+                                        <td></td>
+                                        {/*<td>1.</td>*/}
+                                        {/*<td class="middle-border">e4</td>*/}
+                                        {/*<td>e5</td>*/}
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                  </div>
 
-              onClick={this.uploadWebcamCapture}>
-              Upload
-          </button>
-          <Webcam height={100} audio={false}/>
-      </div>
-    );
+                  <div class="row top-margin">
+                      <div class="col-md-5 col-md-offset-2">
+                          <div class="fen-text">
+                              <span class="fen-span">
+                                  {/*FEN:*/}
+                                  FEN: rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq 0 1
+                              </span>
+                          </div>
+                      </div>
+                      <div class="col-md-3">
+                        <button type="button" class="btn btn-green">Export game</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      );
   }
 }
 
